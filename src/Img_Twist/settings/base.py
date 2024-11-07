bc7b949450e62c9e0abb99faf2814ead3df12ee2
Mahboob-A <iammahboob.a@gmail.com>
2024-11-07 15:39:52 +0530

    feat: Rate Limit Middleware to implement app wise rate limit
    add: TokenBucketRateLimitMiddleware in Token Bucket Algorithm for app wise rate limit

--- src/Img_Twist/settings/base.py
+++ src/Img_Twist/settings/base.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core_apps.products.RateLimiterMiddleware.TokenBucketRateLimitMiddleware", 
 ]
 
 ROOT_URLCONF = "Img_Twist.urls"
