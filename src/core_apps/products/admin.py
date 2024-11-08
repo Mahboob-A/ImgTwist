14c6ae940cbad3f3c4beb56a135f89a07f934517
Mahboob-A <iammahboob.a@gmail.com>
2024-11-08 11:20:18 +0530

    chore: model register in admin

--- src/core_apps/products/admin.py
+++ src/core_apps/products/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from core_apps.products.models import BrandName, Category, Product, ProductImages
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "quantity", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("name",)
+    list_per_page = 10
+    
+@admin.register(ProductImages)
+class ProductImagesAdmin(admin.ModelAdmin):
+    list_display = ("product", "image", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("product",)
+    list_per_page = 10
+    
+@admin.register(Category)
+class BrandNameAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("name",)
+    list_per_page = 10
+    
+@admin.register(BrandName)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "created_at", "updated_at")
+    list_filter = ("created_at", "updated_at")
+    search_fields = ("name",)
+    list_per_page = 10
